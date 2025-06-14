99a2a38be17aa23be97c402d99452a0d405712ab
GIGABYTE <d230261c@sc.edu.my>
2025-06-14 16:18:59 +0800

    test

--- lib/pages/Scan.dart
+++ lib/pages/Scan.dart
@@ -20,25 +20,35 @@ class Scan extends StatelessWidget {
           child: Stack(
             children: [
               Positioned(
-                left: 27,
+                left: 10,
                 top: 133,
                 child: Container(
                   //里面透明的
-                  width: 357,
-                  height: 357,
-                  padding: const EdgeInsets.symmetric(horizontal: 2, vertical: 15),
+                  width: 392,
+                  height: 392,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 2,
+                    vertical: 15,
+                  ),
                   clipBehavior: Clip.antiAlias,
                   decoration: ShapeDecoration(
-                    color: const Color(0x3FBCBCBC),
+                    color: Colors.white.withAlpha(70),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(15),
                     ),
+                    shadows: [
+                      BoxShadow(
+                        color: Color(0x3FBCBCBC),
+                        blurRadius: 4,
+                        offset: Offset(0, 5),
+                        spreadRadius: 0,
+                      ),
+                    ],
                   ),
                   child: Row(
                     mainAxisSize: MainAxisSize.min,
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
-                    spacing: 10,
                     children: [
                       Container(
                         width: 347,
@@ -58,54 +68,40 @@ class Scan extends StatelessWidget {
                 ),
               ),
               Positioned(
-                left: 157,
-                top: 106,
+                left: 165,
+                top: 100,
                 child: Container(
-                  width: 61,
-                  height: 54,
+                  width: 70,
+                  height: 75,
                   decoration: BoxDecoration(
                     image: DecorationImage(
-                      image: NetworkImage("https://placehold.co/61x54"),
+                      image: AssetImage("assets/images/love.png"),
                       fit: BoxFit.cover,
                     ),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Color(0x3F000000),
-                        blurRadius: 4,
-                        offset: Offset(0, 4),
-                        spreadRadius: 0,
-                      )
-                    ],
                   ),
                 ),
               ),
               Positioned(
+                //下面透明的
                 left: 0,
-                top:850,
+                top: 820,
                 child: Container(
                   width: MediaQuery.sizeOf(context).width,
                   height: MediaQuery.sizeOf(context).height,
-                  padding: const EdgeInsets.symmetric(horizontal: 130, vertical: 10),
                   clipBehavior: Clip.antiAlias,
                   decoration: BoxDecoration(color: const Color(0xBFDEDEDE)),
                   child: Stack(
                     children: [
                       Positioned(
-                        left: 0,
-                        top: 1,
                         child: Container(
-                          width: 375,
-                          height: 58,
-                          clipBehavior: Clip.antiAlias,
-                          decoration: BoxDecoration(),
                           child: Stack(
                             children: [
                               Positioned(
-                                left: 83,
-                                top: 8,
+                                left: 70,
+                                top: 15,
                                 child: Container(
-                                  width: 209,
-                                  height: 43,
+                                  width: 270,
+                                  height: 70,
                                   decoration: ShapeDecoration(
                                     color: Colors.white,
                                     shape: RoundedRectangleBorder(
@@ -119,33 +115,27 @@ class Scan extends StatelessWidget {
                                   child: Stack(
                                     children: [
                                       Positioned(
-                                        left: 24,
-                                        top: 5,
-                                        child: Container(
-                                          width: 30,
-                                          height: 33,
-                                          decoration: BoxDecoration(
-                                            image: DecorationImage(
-                                              image: NetworkImage("https://placehold.co/30x33"),
-                                              fit: BoxFit.contain,
-                                            ),
-                                          ),
+                                        left: 15,
+                                        top: 8,
+                                        child: Icon(
+                                            Icons.upload,
+                                                size: 50,
                                         ),
                                       ),
                                       Positioned(
-                                        left: 39,
-                                        top: 0,
+                                        left: 80,
+                                        top: 5,
                                         child: SizedBox(
                                           width: 170,
-                                          height: 43,
+                                          height: 80,
                                           child: Text(
                                             'Upload',
                                             textAlign: TextAlign.center,
                                             style: TextStyle(
                                               color: const Color(0xFF828282),
-                                              fontSize: 16,
+                                              fontSize: 35,
                                               fontFamily: 'Inter',
-                                              fontWeight: FontWeight.w400,
+                                              fontWeight: FontWeight.w500,
                                               height: 1.40,
                                             ),
                                           ),
@@ -164,11 +154,11 @@ class Scan extends StatelessWidget {
                 ),
               ),
               Positioned(
-                left: 69,
-                top: 515,
+                left: 60,
+                top: 620,
                 child: Container(
-                  width: 238,
-                  height: 75,
+                  width: 300,
+                  height: 100,
                   decoration: ShapeDecoration(
                     color: Colors.white,
                     shape: RoundedRectangleBorder(
@@ -178,31 +168,11 @@ class Scan extends StatelessWidget {
                   child: Stack(
                     children: [
                       Positioned(
-                        left: 91,
-                        top: 15,
-                        child: Container(
-                          width: 56,
-                          height: 46,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: NetworkImage("https://placehold.co/56x46"),
-                              fit: BoxFit.contain,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        left: 202,
-                        top: -30,
-                        child: Container(
-                          width: 72,
-                          height: 59,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: NetworkImage("https://placehold.co/72x59"),
-                              fit: BoxFit.contain,
-                            ),
-                          ),
+                        left: 105,
+                        top: 12,
+                        child: Icon(
+                          Icons.camera_alt_outlined,
+                          size: 80,
                         ),
                       ),
                     ],
@@ -210,29 +180,43 @@ class Scan extends StatelessWidget {
                 ),
               ),
               Positioned(
-                left: 33,
-                top: 560,
+                left: 280,
+                top: 460,
                 child: Container(
-                  width: 72,
-                  height: 59,
+                  width: 140,
+                  height: 300,
                   decoration: BoxDecoration(
                     image: DecorationImage(
-                      image: NetworkImage("https://placehold.co/72x59"),
+                      image: AssetImage("assets/images/cat.png"),
                       fit: BoxFit.contain,
                     ),
                   ),
                 ),
               ),
               Positioned(
-                left: 7.50,
-                top: 16,
+                left: 0,
+                top: 560,
                 child: Container(
-                  width: 360,
+                  width: 150,
+                  height: 300,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/Cat3.png"),
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 15,
+                top: 20,
+                child: Container(
+                  width: 380,
                   height: 65,
                   padding: const EdgeInsets.symmetric(vertical: 8),
                   clipBehavior: Clip.antiAlias,
                   decoration: ShapeDecoration(
-                    color: Colors.white.withValues(alpha: 191),
+                    color: Colors.white.withAlpha(191),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(15),
                     ),
@@ -244,11 +228,12 @@ class Scan extends StatelessWidget {
                     spacing: 10,
                     children: [
                       Container(
-                        width: 37,
-                        height: 37,
+                        margin: EdgeInsets.only(left:0),
+                        width: 45,
+                        height: 45,
                         clipBehavior: Clip.antiAlias,
                         decoration: ShapeDecoration(
-                          color: Colors.white.withValues(alpha: 128),
+                          color: Colors.white.withAlpha(128),
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(15),
                           ),
@@ -258,22 +243,24 @@ class Scan extends StatelessWidget {
                               blurRadius: 4,
                               offset: Offset(0, 4),
                               spreadRadius: 0,
-                            )
+                            ),
                           ],
                         ),
                         child: Stack(
                           children: [
                             Positioned(
                               left: 7,
-                              top: 7,
-                              child: Container(
-                                width: 24,
-                                height: 24,
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                    image: NetworkImage("https://placehold.co/24x24"),
-                                    fit: BoxFit.contain,
-                                  ),
+                              top: 8,
+                              child: GestureDetector(
+                                onTap: () {
+                                  Navigator.pushReplacementNamed(
+                                    context,
+                                    '/Home',
+                                  );
+                                },
+                                child: Icon(
+                                  Icons.arrow_back,
+                                  size: 30,
                                 ),
                               ),
                             ),
@@ -284,7 +271,7 @@ class Scan extends StatelessWidget {
                         width: 244,
                         clipBehavior: Clip.antiAlias,
                         decoration: ShapeDecoration(
-                          color: Colors.white.withValues(alpha: 128),
+                          color: Colors.white.withAlpha(128),
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(15),
                           ),
@@ -294,23 +281,19 @@ class Scan extends StatelessWidget {
                               blurRadius: 4,
                               offset: Offset(0, 4),
                               spreadRadius: 0,
-                            )
+                            ),
                           ],
                         ),
                         child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          spacing: 10,
                           children: [
                             Container(
-                              width: double.infinity,
+                              width: MediaQuery.sizeOf(context).width,
                               height: 48,
                               child: Stack(
                                 children: [
                                   Positioned(
                                     left: 0,
-                                    top: 0,
+                                    top: 15,
                                     child: SizedBox(
                                       width: 244,
                                       height: 48,
@@ -324,7 +307,15 @@ class Scan extends StatelessWidget {
                                           fontWeight: FontWeight.w700,
                                           height: 1,
                                           letterSpacing: 3.30,
-                                          shadows: [Shadow(offset: Offset(0, 4), blurRadius: 4, color: Color(0xFF000000).withOpacity(0.25))],
+                                          shadows: [
+                                            Shadow(
+                                              offset: Offset(0, 4),
+                                              blurRadius: 4,
+                                              color: Color(
+                                                0xFF000000,
+                                              ).withOpacity(0.25),
+                                            ),
+                                          ],
                                         ),
                                       ),
                                     ),
@@ -334,25 +325,6 @@ class Scan extends StatelessWidget {
                             ),
                           ],
                         ),
-                      ),
-                      Container(
-                        width: 37,
-                        height: 37,
-                        clipBehavior: Clip.antiAlias,
-                        decoration: ShapeDecoration(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          shadows: [
-                            BoxShadow(
-                              color: Color(0x3F000000),
-                              blurRadius: 4,
-                              offset: Offset(0, 4),
-                              spreadRadius: 0,
-                            )
-                          ],
-                        ),
-                        child: Stack(),
                       ),
                     ],
                   ),
